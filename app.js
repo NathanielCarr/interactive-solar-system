9b577d7854825d3eec3b099802c26a984b37c53d
Nathaniel Carr <carr0170@mylaurier.ca>
2019-11-14 17:29:58 -0500

    The planets now orbit the sun.
    Co-authored-by: Brendan Whelan <whebre@gmail.com>
    Co-authored-by: Tim <123timmcgill@gmail.com>

--- app.js
+++ app.js
@@ -1,70 +1,75 @@
 // const THREE = require("./js/three");
 
 // All distances are in kilometres.
+const ORBIT_SPEED = 1;
 const RADIUS_SCALE = 1 / 57910000;
 const SOLAR_DISTANCE_SCALE = 1 / 57910000;
 const PLANET_PROPERTIES = {
+    // RADIUS is the real radius (km)
+    // NOSCALE_RADIUS is a dramatized radius.
+    // SOLAR_DISTANCE is distance from the sun (km).
+    // TEXTURE is the location where the texture is found.
     SUN: {
         RADIUS: 695508,
         NOSCALE_RADIUS: 1.2,
         SOLAR_DISTANCE: 0,
-        COLOR: 0xffff00,
+        DAYS_PER_ORBIT: 0,
         TEXTURE: 'assets/1K/sun.jpg'
     },
     MERCURY: {
         RADIUS: 2440,
         NOSCALE_RADIUS: 0.15,
         SOLAR_DISTANCE: 57910000,
-        COLOR: 0xc0c0c0,
+        DAYS_PER_ORBIT: 87.97,
         TEXTURE: 'assets/1K/mercury.jpg'
     },
     VENUS: {
         RADIUS: 6052,
         NOSCALE_RADIUS: 0.3,
         SOLAR_DISTANCE: 108200000,
-        COLOR: 0xffffcc,
+        DAYS_PER_ORBIT: 224.70,
         TEXTURE: 'assets/1K/venus_surface.jpg'
     },
     EARTH: {
         RADIUS: 6378,
         NOSCALE_RADIUS: 0.3,
         SOLAR_DISTANCE: 149600000,
-        COLOR: 0x0080ff,
+        DAYS_PER_ORBIT: 365.26,
         TEXTURE: 'assets/1K/earth_daymap.jpg'
     },
     MARS: {
         RADIUS: 3397,
         NOSCALE_RADIUS: 0.25,
         SOLAR_DISTANCE: 227900000,
-        COLOR: 0x930000,
+        DAYS_PER_ORBIT: 686.98,
         TEXTURE: 'assets/1K/mars.jpg'
     },
     JUPITER: {
         RADIUS: 71492,
         NOSCALE_RADIUS: 1,
         SOLAR_DISTANCE: 778500000,
-        COLOR: 0xfcc92e,
+        DAYS_PER_ORBIT: 4332.82,
         TEXTURE: 'assets/1K/jupiter.jpg'
     },
     SATURN: {
         RADIUS: 60268,
         NOSCALE_RADIUS: 0.9,
         SOLAR_DISTANCE: 1434000000,
-        COLOR: 0xc8c800,
+        DAYS_PER_ORBIT: 10755.70,
         TEXTURE: 'assets/1K/saturn.jpg'
     },
     URANUS: {
         RADIUS: 25559,
         NOSCALE_RADIUS: 0.6,
         SOLAR_DISTANCE: 2871000000,
-        COLOR: 0xcaffff,
+        DAYS_PER_ORBIT: 30687.15,
         TEXTURE: 'assets/1K/uranus.jpg'
     },
     NEPTUNE: {
         RADIUS: 24766,
         NOSCALE_RADIUS: 0.6,
         SOLAR_DISTANCE: 4495000000,
-        COLOR: 0x007fff,
+        DAYS_PER_ORBIT: 60190.03,
         TEXTURE: 'assets/1K/neptune.jpg'
     }
 };
@@ -94,14 +99,23 @@ async function main() {
     let planets = {};
     for (let planetName of Object.keys(PLANET_PROPERTIES)) {
 
+        // Create the planet variable.
         planets[planetName.toLowerCase()] = {};
         let planet = planets[planetName.toLowerCase()];
+        planet = Object.assign(planet, PLANET_PROPERTIES[planetName]);
 
-        planet.geometry = new THREE.SphereGeometry(PLANET_PROPERTIES[planetName].NOSCALE_RADIUS, 32, 32);
-        planet.texture = await asyncLoadTexture(new THREE.TextureLoader(), PLANET_PROPERTIES[planetName].TEXTURE);
-        planet.material = new THREE.MeshBasicMaterial({ map: planet.texture});
+        // Define the attributes of the planet variable.
+        planet.geometry = new THREE.SphereGeometry(planet.NOSCALE_RADIUS, 32, 32);
+        planet.texture = await asyncLoadTexture(new THREE.TextureLoader(), planet.TEXTURE).catch((err) => {
+            console.error(err);
+        });
+        planet.material = new THREE.MeshBasicMaterial({
+            map: planet.texture
+        });
         planet.mesh = new THREE.Mesh(planet.geometry, planet.material);
-        planet.mesh.translateX(PLANET_PROPERTIES[planetName].SOLAR_DISTANCE * SOLAR_DISTANCE_SCALE);
+        planet.mesh.translateX(planet.SOLAR_DISTANCE * SOLAR_DISTANCE_SCALE);
+
+        // Add the planet.
         scene.add(planet.mesh);
     }
 
@@ -122,19 +136,26 @@ async function main() {
         }
     };
 
+    // Move to the default camera position.
     camera.position.y = 125;
     camera.position.z = 125;
     camera.position.x = PLANET_PROPERTIES.NEPTUNE.SOLAR_DISTANCE * SOLAR_DISTANCE_SCALE + PLANET_PROPERTIES.NEPTUNE.NOSCALE_RADIUS + 100;
     camera.lookAt(planets.sun.mesh.position);
 
-    renderer.render(scene, camera);
-
-    // function animate() {
-    //     requestAnimationFrame(animate);
-    //     sun.rotation.x += 0.01;
-    //     renderer.render(scene, camera);
-    // }
-    // animate();
+    // Set animation function.
+    function animate() {
+        requestAnimationFrame(animate);
+        let time = Date.now();
+        for (let planetName of Object.keys(planets)) {
+            let planet = planets[planetName];
+            if (planetName !== "sun") {
+                planet.mesh.position.set(Math.cos(time * (ORBIT_SPEED / planet.DAYS_PER_ORBIT) + 10) * (SOLAR_DISTANCE_SCALE * planet.SOLAR_DISTANCE), 0, Math.sin(time * (ORBIT_SPEED / planet.DAYS_PER_ORBIT) + 10) * (SOLAR_DISTANCE_SCALE * planet.SOLAR_DISTANCE));
+            }
+        }
+        // sun.rotation.x += 0.01;
+        renderer.render(scene, camera);
+    }
+    animate();
 }
 
 main();
